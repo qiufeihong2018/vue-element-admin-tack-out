6fc5fc47ee9bb65d38386b055542134303a8f16a
qiufeihong <qiufeihong2018@126.com>
2019-02-21 17:28:22 +0800

    ERROR in static/js/5.5963a04851d9fffdae78.js from UglifyJs

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -45,7 +45,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'),resolve('node_modules/time-formater/timeformater.js')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
